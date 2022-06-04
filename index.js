9bf9d316ce6e060866ad19061183d2c8a6176e1b
Ilori Gbenga <56758805+dangbenzy@users.noreply.github.com>
2022-06-04 21:17:13 +0000

    Put in the right code

--- index.js
+++ index.js
@@ -1,35 +1,21 @@
-let groupA= ["Mike","Ola", "Femi", "Prince", "Nnamdi"]
-let groupB= ["Adesewa", "Adaeze", "Erica", "Titilope"]
-
-let places= ["Nairobi", "Miami", "Lagos", "Bermuda"]
-
-
-
-
-function getRandomNum(a){
-    let randomNumber= Math.floor(Math.random()*a)
-    return randomNumber
+let points= 0
+function getRandomNum(min,max){
+    let randomNumber= Math.floor((Math.random()*max)+ min);
+  return randomNumber 
     }
-function searchRandom(count, arr){
-  let answer = [], counter = 0;
- 
-  while(counter < count){
-    let rand = arr[Math.floor(Math.random() * arr.length)];
-    if(!answer.some(an => an === rand)){
-      answer.push(rand);
-      counter++;
-    }
+
+
+function guess(min, max, myGuess){
+  if (myGuess === getRandomNum(min,max)){
+    max++
+    points++;
+    result= 'Correct! You earned '+points+' point';
   }
-  
-  return answer;
+  else{
+    result='Try again'
+  }
+  console.log(result)
 }
 
+guess(1, 2, 1)
 
-groupB[getRandomNum(groupB.length)]
-places[getRandomNum(places.length)]
-
-for(i=0; i<5; i++){
-  
-  console.log(searchRandom(5,groupA)[i]+" and "+groupB[getRandomNum(groupB.length)]+" will be going on a dinner date at "+places[getRandomNum(places.length)])
-
-              }
